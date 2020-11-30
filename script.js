35ed572f326540e1f3bd15c5adab5b58f1fe8175
Anamika Gurung <anamika.gurung@gmail.com>
2020-11-30 12:26:46 +1100

    challenge-4 completed

--- script.js
+++ script.js
@@ -98,8 +98,10 @@ console.log(all_buttons);
 
 var copyAllButtons=[];
 for (let i=0; i<all_buttons.length; i++){
-    copyAllButtons.push(all_buttons[i]);
+    copyAllButtons.push(all_buttons[i] .classList[1]);
 }
+console.log(copyAllButtons);
+
 function buttonColorChange(buttonThingy){
     if(buttonThingy.value==='red'){
         buttonsRed();
@@ -110,6 +112,35 @@ function buttonColorChange(buttonThingy){
     }else if (buttonThingy.value==='random'){
         randomColors();
     }
+}
+function buttonsRed(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+function buttonsGreen(){
+    for(let i=0; i< all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+function buttonColorReset(){
+    for(let i=0; i<all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+
+    }
+}
+function randomColors(){
+    var choices= ['btn-primary','btn-danger','btn-success','btn-warning']
+
+    for(let i=0; i<all_buttons.length; i++){
+        var randomNumber= Math.floor(Math.random()*4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+}
+
 }
 
 
